a96a702c4fee7bdd6d71912d80842242d3b8dc14
0xwilliamli <williamli.elite@gmail.com>
2024-02-23 13:54:38 -0500

    Deploy random fleek function to ipds

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,6 +1,27 @@
+// @ts-nocheck
 import { AwsStackV2 } from "./stack";
+import { FleekSdk, PersonalAccessTokenService } from "@fleekxyz/sdk";
 import * as cdk from 'aws-cdk-lib';
+import * as fs from 'node:fs/promises';
 require('dotenv').config();
+
+const deployFleekFunction = async () => {
+    
+    const fleekPat = new PersonalAccessTokenService({personalAccessToken: process.env.FLEEK_PAT!,projectId: process.env.FLEEK_PROJECT_ID});
+    const fleekSdk = new FleekSdk({accessTokenService: fleekPat});
+    
+    const fleekFunction = `const main = (params) => {\n  return ${Math.floor(Math.random() * 1000)}\n}`;
+
+
+    const result = await fleekSdk.ipfs().add({
+        path: "function",
+        content: fleekFunction
+    })
+
+await fs.writeFile("./ts/CID.txt", new TextEncoder().encode(result.cid));
+}
+
+deployFleekFunction();
 
 const app = new cdk.App();
 
